4d1317865e6b74db6d712264bc89fd2ef9402995
Oren Fisk <oren.fisk@bazaarvoice.com>
2016-12-24 22:39:44 -0600

    implemented messageBus with local, global and admin command registration; used messagebus to handle gender setting in training room

---
+++ messaging/messagebus.py
@@ -0,0 +1,53 @@
+ADMIN_USER = 'U10F84U2J'
+
+class MessageBus :
+    adminCommands = {}
+    pendingCommandsByUser = {}
+    registeredUsers = {}
+    globalCommands = {}
+
+    def __init__(self) :
+        globalCommands = {}
+        adminCommands = {}
+        pendingCommandsByUser = {}
+        registeredUsers = {}
+
+    def handleMessage(self, rawMessage) :
+        message = None
+        userId = None
+        channel = None
+        messageType = None
+
+        if (len(rawMessage) > 0) :
+            channel = rawMessage.get('channel', None)
+            message = rawMessage.get('text', None)
+            userId = rawMessage.get('user', None)
+            messageType = rawMessage.get('type', None)
+        if (userId == None) :
+            return
+
+        targetUser = self.registeredUsers.get(userId, None)
+        if (self.adminCommands.get(message, None) != None and userId == ADMIN_USER) :
+            self.adminCommands[message]()
+            return
+        elif (self.registeredUsers.get(userId, None) == None or message.startswith("@slackmud")) :
+            if (self.globalCommands.get(message, None) != None) :
+                self.globalCommands[message](userId)
+                return
+        pendingCommand = self.pendingCommandsByUser.get(userId, None)
+        if (pendingCommand == None) :
+            return "COMMAND_NOT_FOUND"
+        self.pendingCommandsByUser[userId] = None
+        pendingCommand(targetUser, message)
+
+    def registerAdminCommand(self, message, command) :
+        self.adminCommands[message] = command
+
+    def registerGlobalCommand(self, message, command) :
+        self.globalCommands[message] = command
+
+    def registerUser(self, user) :
+        self.registeredUsers[user["id"]] = user
+
+    def registerCommandForUser(self, user, command) :
+        self.pendingCommandsByUser[user["id"]] = command
